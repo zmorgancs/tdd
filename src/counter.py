84132cfc87033e68129030aed06ecc71e580acf3
zmorgancs <122938637+zmorgancs@users.noreply.github.com>
2024-02-09 17:20:08 -0800

    Corrected E302 error in counter.py
    flake8 encountered issues with the amount of blank lines in certain parts of the file so they were added.

--- src/counter.py
+++ src/counter.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 COUNTERS = {}
 
+
 # We will use the app decorator and create a route called slash counters.
 # specify the variable in route <name>
 # let Flask know that the only methods that is allowed to called
@@ -19,6 +20,7 @@ def create_counter(name):
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
 
+
 #    1. Create a route for method PUT on endpoint /counters/<name>.
 #    2. Create a function to implement that route.
 #    3. Increment the counter by 1.
@@ -29,9 +31,10 @@ def update_counter(name):
     app.logger.info(f"Request to update counter: {name}")
     global COUNTERS
     if name in COUNTERS:
-        COUNTERS[name] += 1             # Increment counter by 1
-        return {name: COUNTERS[name]}, status.HTTP_200_OK   # return the new counter and a 200_OK return code
+        COUNTERS[name] += 1  # Increment counter by 1
+        return {name: COUNTERS[name]}, status.HTTP_200_OK  # return the new counter and a 200_OK return code
     return {"Message": f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND  # If no counter, return error msg
+
 
 @app.route('/counters/<name>', methods=['GET'])  # create route for GET
 def read_counter(name):
@@ -40,5 +43,4 @@ def read_counter(name):
     global COUNTERS
     if name in COUNTERS:
         return {name: COUNTERS[name]}, status.HTTP_200_OK  # return the counter and a 200_OK return code
-    return {"Message": f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND   # If no counter, return error msg
-
+    return {"Message": f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND  # If no counter, return error msg
